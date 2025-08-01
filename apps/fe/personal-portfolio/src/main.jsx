ba78c527ddcefa90cd3251a4c5053b54743422c0
shinsouhitoshi1203 <sinnohsatoshi.1203@gmail.com>
2025-08-01 21:19:38 +0700

    chore: init workspace and try creating personal-portfolio project

---
+++ apps/fe/personal-portfolio/src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <StrictMode>
+        <h1>Hello World</h1>
+    </StrictMode>
+);
---
+++ apps/fe/personal-portfolio/vite.config.ts
@@ -0,0 +1,32 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(() => ({
+    root: __dirname,
+    cacheDir: '../../../node_modules/.vite/apps/fe/personal-portfolio',
+    server: {
+        port: 8080,
+        host: 'localhost',
+    },
+    preview: {
+        port: 8080,
+        host: 'localhost',
+    },
+    plugins: [react()],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    build: {
+        outDir: './dist',
+        emptyOutDir: true,
+        reportCompressedSize: true,
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
+    define: {
+        'import.meta.vitest': undefined,
+    },
+}));
